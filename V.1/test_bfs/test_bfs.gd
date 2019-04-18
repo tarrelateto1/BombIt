8cd3e3496f2b1397b8312fa3e71c6566d81b3fc8
Suphawinee <swu59296@outlook.com>
2019-04-18 15:52:19 +0700

    bfs v1

--- V.1/test_bfs/test_bfs.gd
+++ V.1/test_bfs/test_bfs.gd
@@ -4,6 +4,7 @@ extends Node2D
 # var a = 2
 # var b = "textvar"
 onready var start = get_node("KinematicBody2sD")
+var queue =[]
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -13,17 +14,47 @@ func _ready():
 	print("start ",start.get_position())
 #	start.position = Vector2(32,96)
 #	print("next ",start.get_position())
-	find_neighbors(start)
+	var frontier = []
+	frontier.append(10)
+	frontier.append(11)
+	print(frontier.size())
+	print(frontier[1])
+#	print(Vector2(10,10))
+	
+	var a =find_neighbors(start)
+	
 	pass
 
 func _process(delta):
 	# Called every frame. Delta is time since last frame.
 	# Update game logic here.
 #	find_neighbors(1,2)
+	test(start)
+	pass
+func test(start):
+	var current
+	var frontier = []
+	var currentleft = 0 
+	frontier.append(start)
+	var visits = [] 
+	visits.append(null)
+	while visits.size() >0:
+		current = frontier.pop_front()
+#		currentleft +=1
+		for next in find_neighbors(current):
+			for visit in visits :
+				if next != visit:
+					frontier.append(next)
+					visits.append(current)
+#			if next != in
+			
+			
+			
+	
 	pass
 
-
 func bfs():
+	
 	pass
 	
 func find_neighbors(start):
@@ -32,6 +63,16 @@ func find_neighbors(start):
 	print("left :",start.position+Vector2(-32,0))
 	print("bot  :",start.position+Vector2(0,32))
 	print("right  :",start.position+Vector2(32,0))
+	var frontier =[]
+	var Ptop =KinematicBody2D.new()
+	
+	frontier.append(start.position+Vector2(0,-32))
+	frontier.append(start.position+Vector2(-32,0))
+	frontier.append(start.position+Vector2(0,32))
+	frontier.append(start.position+Vector2(32,0))
+	print(frontier)
+	
+	return frontier
 	pass
 	
 	
