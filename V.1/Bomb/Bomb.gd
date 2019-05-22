b5191e171ef05f6ef45df5c94647ccd2a9bbb44a
Jirawat promsee <swu59898@outlook.com>
2019-05-23 02:13:32 +0700

    delete comment

--- V.1/Bomb/Bomb.gd
+++ V.1/Bomb/Bomb.gd
@@ -1,10 +1,6 @@
 extends Area2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-#var collision_H
-#var collision_V
+
 var time_boom =0
 var count_bomb_top = 0
 var count_bomb_right = 0
@@ -26,67 +22,19 @@ var destory = []
 var idx = 0
 
 
-#var img = preload("res://Bomb/fire.tscn")
+
 func _ready():
 	
-	Singleton.list_bomb.append(self)
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	
-#	print(get_node("CollisionShape2D"))
-#	collision_H = get_node("Collision_H")
-#	collision_V = get_node("Collision_V")
-	pass
 
-#func set1():
-#
-#	pass
+	
+	pass
 
 
 func _on_Timer_timeout():	
 	time_boom +=0.1
-#	print("bomb position : ",get_position())
-#	print(time_boom)
+
 	if time_boom > 2 && time_boom <3:
-#		Horizontal up
-#		var shape_H_top = RectangleShape2D.new()
-#		shape_H_top.set_extents(Vector2(10,10))
-#		var collision_H_top = CollisionShape2D.new()
-#		Horizontal down
-#		var shape_H_down = RectangleShape2D.new()
-#		shape_H_down.set_extents(Vector2(10,10))
-#		var collision_H_down = CollisionShape2D.new()
-#		Verticle  LEFT
-#		var shape_V_LEFT = RectangleShape2D.new()
-#		shape_V_LEFT.set_extents(Vector2(10,10))
-#		var collision_V_LEFT = CollisionShape2D.new()
-#		Verticle Right
-#		var shape_V_RIGHT = RectangleShape2D.new()
-#		shape_V_RIGHT.set_extents(Vector2(10,10))
-#		var collision_V_RIGHT = CollisionShape2D.new()
-#
 
-		
-#		print(shape)
-
-#		collision_V_RIGHT.set_shape(shape_V_RIGHT)
-#		collision_V_RIGHT.position.x = 32
-		
-#		collision_V_LEFT.set_shape(shape_V_LEFT)
-#		collision_V_LEFT.position.x = -32
-#
-#		collision_H_down.set_shape(shape_H_down)
-#		collision_H_down.position.y = 32
-#
-#		collision_H_top.set_shape(shape_H_top)
-#		collision_H_top.position.y = -32
-		
-		
-
-#		add_child(collision_H_top)
-#		add_child(collision_H_down)
-#		add_child(collision_V_LEFT)
-#		add_child(collision_V_RIGHT)
 		# top
 		if count_bomb_top < max_bomb && find_wall_top:
 #			print(count_bomb_right)
@@ -95,21 +43,19 @@ func _on_Timer_timeout():
 			var collision_H_TOP = CollisionShape2D.new()
 			collision_H_TOP.set_shape(shape_H_TOP)
 			collision_H_TOP.position.y = -32*(count_bomb_top+1)
-#			shape.append(collision_V_RIGHT)
+
 			collect_top_area.append(count_area)
-#			img.position.y = -32*(count_bomb_top+1)
+
 			var img = Sprite.new()
 			img.texture = load("res://Bomb/fire.jpg")
 			img.position.y = -32*(count_bomb_top+1)
 			img.set_scale(Vector2(0.035556,0.033333))
 			add_child(img)
-#			add_child(img)
-#			if(colision_wall):	
 #			print(collision_H_TOP)
 			add_child(collision_H_TOP)
 			count_area+=1	
 			count_bomb_top+=1
-#			shape[0].position.x = 32*2
+
 		# RIGHT
 		if count_bomb_right < max_bomb && find_wall_right:
 #			print(count_bomb_right)
@@ -118,10 +64,7 @@ func _on_Timer_timeout():
 			var collision_V_RIGHT = CollisionShape2D.new()
 			collision_V_RIGHT.set_shape(shape_V_RIGHT)
 			collision_V_RIGHT.position.x = 32*(count_bomb_right+1)
-#			shape.append(collision_V_RIGHT)
-#			if(colision_wall):	
 			collect_right_area.append(count_area)
-#			print(collect_right_area)
 			var img = Sprite.new()
 			img.texture = load("res://Bomb/fire.jpg")
 			img.position.x = 32*(count_bomb_right+1)
@@ -130,7 +73,7 @@ func _on_Timer_timeout():
 			add_child(collision_V_RIGHT)
 			count_area+=1	
 			count_bomb_right+=1
-#			shape[0].position.x = 32*2
+
 		#DOWN
 		if count_bomb_down < max_bomb && find_wall_down:
 #			print(count_bomb_right)
@@ -139,8 +82,6 @@ func _on_Timer_timeout():
 			var collision_H_DOWN = CollisionShape2D.new()
 			collision_H_DOWN.set_shape(shape_H_DOWN)
 			collision_H_DOWN.position.y = 32*(count_bomb_down+1)
-#			shape.append(collision_V_RIGHT)
-#			if(colision_wall):	
 			collect_down_area.append(count_area)
 			
 			var img = Sprite.new()
@@ -161,10 +102,7 @@ func _on_Timer_timeout():
 			collision_V_LEFT.set_shape(shape_V_LEFT)
 			collision_V_LEFT.position.x = -32*(count_bomb_left+1)
 			shape.append(collision_V_LEFT)
-#			print(shape)
-#			if(colision_wall):	
 			collect_left_area.append(count_area)
-#			print("collect in left ",collect_left_area)
 			var img = Sprite.new()
 			img.texture = load("res://Bomb/fire.jpg")
 			img.position.x = -32*(count_bomb_left+1)
@@ -178,34 +116,7 @@ func _on_Timer_timeout():
 	#destroy boom
 	elif time_boom >3:
 #		self_player.number_current_bomb -=1
-#		var idx = 0
-#		print(destory)
-##		queue_free()
-#		for D in destory:
-###		for L in Singleton.list_rock:
-###
-###			for D in destory:
-#			for L in Singleton.list_rock:
-##
-###				print(D,"and",L)
-####				print("rocks",Singleton.list_rock)
-####				print("destory",destory)
-###
-#				if L == D:
-#					print(D,"and",L)
-###					print("โดนหิน")
-###					print(L)
-#					Singleton.list_rock.remove(idx)
-#					L.queue_free()
-####					if L != null:
-###					Singleton.list_rock.remove(idx)
-###					L.queue_free()
-####						Singleton.list_rock.remove(idx)
-#			idx +=1
-#		print(Singleton.list_rock)
-#		print(Singleton.list_bomb)
-#		get_tree().root.print_tree_pretty()
-#		print("ระเบิดแล้ว")
+
 		queue_free()
 		
 	
@@ -215,7 +126,7 @@ func _on_Timer_timeout():
 func _on_Bomb_body_entered(body):
 	var idx = 0
 	destory.append(body)
-#	print("เจอ ",destory)
+
 	
 	for L in Singleton.list_rock:
 	
@@ -224,49 +135,33 @@ func _on_Bomb_body_entered(body):
 #			print(L)
 			Singleton.list_rock.remove(idx)
 			L.queue_free()
-#			print(Singleton.list_rock)
+
 		idx +=1
-#	print(Singleton.list_rock)	
-	
-#	for L in Singleton.list_wall:
-#		if L == body:
-#			find_wall_left = false
-#			print("โดนกำแพง")
-#			print(L)
-#			L.queue_free()
-#	print(Singleton.a)
-#	body.queue_free()
-#	print(body)
+
 	pass # replace with function body
 
 
 
 
 func _on_Bomb_body_shape_entered(body_id, body, body_shape, area_shape):
-#	print(body)
-#	print("area_shape : ",area_shape)
-#	print(shape)
-#	if area_shape == 0 :
+
 	for k in collect_top_area:
 		for L in Singleton.list_wall:
 			if L == body&& k == area_shape:
 				find_wall_top = false
-#				print("โดนกำแพง")
-#				print(L)	
+
 #	if area_shape == 1 :
 	for k in collect_right_area:
 		for L in Singleton.list_wall:
 			if L == body && k == area_shape:
 				find_wall_right = false
-#				print("โดนกำแพง")
-#				print(L)	
+
 #	if area_shape == 2 :
 	for k in collect_down_area:
 		for L in Singleton.list_wall:
 			if L == body && k == area_shape:
 				find_wall_down = false
-#				print("โดนกำแพง")
-#				print(L)	
+
 				
 #	if area_shape == 3 :
 	for k in collect_left_area:
@@ -276,16 +171,12 @@ func _on_Bomb_body_shape_entered(body_id, body, body_shape, area_shape):
 #				print("โดนกำแพงซ้าย ที่",area_shape ,"กับ ",k)
 #				print(L)	
 				
-#	check_wall(collect_top_area,find_wall_top,body,area_shape)
-#	check_wall(collect_right_area,find_wall_right,body,area_shape)
-#	check_wall(collect_down_area,find_wall_down,body,area_shape)
-#	check_wall(collect_left_area,find_wall_left,body,area_shape)
 
+# ยังไม่ได้ใช้
 func check_wall(collect,find_wall,body,area_shape):
 	for k in collect:
 		for L in Singleton.list_wall:
 			if L == body&& k ==area_shape:
 				find_wall = false
-#				print("โดนกำแพง ที่",area_shape ,"กับ ",k)
-#				print(L)	
+
 
--- V.1/Main.gd
+++ V.1/Main.gd
@@ -7,7 +7,6 @@ extends Node2D
 
 
 var spritedir = "down"
-#var Loadplayer = preload("res://Player/Player.tscn")
 var Bomb = preload("res://Bomb/Bomb.tscn")
 var Bomb1 = preload("res://Bomb/Bomb.tscn")
 var list_bomb = []
@@ -23,48 +22,35 @@ func _ready():
 	# Initialization here
 	Singleton.player1 = $Player
 	Singleton.player2 = $Player2
-#	player.position = Vector2(170,70)
-#	print(player.get_node("Sprite"))
-#	add_child(player)
-#	get_tree().root.print_tree_pretty()
-#	player.print_tree_pretty()
 	Singleton.player.append(player)
-#	Singleton.player.append(player)
-#	var Rocks = get_node("rocks")
 	
 	for N in get_node("rocks").get_children():
 		Singleton.list_rock.append(N)
-#	Singleton.list_rock[0].queue_free()
-#	print(Singleton.list_rock)
+
 	Singleton.grid = get_node("TileMap")
 	grid = get_node("TileMap")
 	for N in get_node("wall").get_children():
 		Singleton.list_wall.append(N)
-	
-#	for n in Singleton.list_rock:
-#		print(n)
-	
-#	speed = Singleton.speed
-	
+
 
 func _process(delta):
 
-#	$Control/Label.text = "Player 1 = position " + String($Player.get_position().x) + " " + String($Player.get_position().y) 
+
 	var v = Vector2()
 	
-#	if Input.is_key_pressed(KEY_LEFT):
+
 	if Input.is_action_pressed("ui_left"):
 		v.x = -Singleton.player1.speed
 		spritedir = "left"
-#	elif Input.is_key_pressed(KEY_RIGHT):
+
 	elif Input.is_action_pressed("ui_right"):
 		v.x = Singleton.player1.speed
 		spritedir = "right"
-#	elif Input.is_key_pressed(KEY_UP):
+
 	elif Input.is_action_pressed("ui_up"):
 		v.y = -Singleton.player1.speed
 		spritedir = "up"
-#	elif Input.is_key_pressed(KEY_DOWN):
+
 	elif Input.is_action_pressed("ui_down"):
 		v.y = Singleton.player1.speed
 		spritedir = "down"
@@ -73,26 +59,22 @@ func _process(delta):
 	
 	var ds = v * delta
 	if ds != Vector2(0,0):
-#		if(Singleton.move_c):
-			$Player.move_and_collide(v*delta)
+
+		$Player.move_and_collide(v*delta)
 			
 			
 	var v1 = Vector2()
 	
 	if Input.is_key_pressed(KEY_LEFT):
-#	if Input.is_action_pressed("ui_left"):
 		v1.x = -Singleton.player2.speed
 		spritedir = "left"
 	elif Input.is_key_pressed(KEY_RIGHT):
-#	elif Input.is_action_pressed("ui_right"):
 		v1.x = Singleton.player2.speed
 		spritedir = "right"
 	elif Input.is_key_pressed(KEY_UP):
-#	elif Input.is_action_pressed("ui_up"):
 		v1.y = -Singleton.player2.speed
 		spritedir = "up"
 	elif Input.is_key_pressed(KEY_DOWN):
-#	elif Input.is_action_pressed("ui_down"):
 		v1.y = Singleton.player2.speed
 		spritedir = "down"
 		
@@ -100,114 +82,61 @@ func _process(delta):
 	
 	var ds1 = v1 * delta
 	if ds1 != Vector2(0,0):
-#		if(Singleton.move_c):
-			$Player2.move_and_collide(v1*delta)
-		
-#		print($Player.get_position())
-#		anim_switch("walk")
-#	print("x= ",$Player.get_position().x ,"  y=",$Player.get_position().y)
-#	print($Player.get_position().x)
-#	print($Player.x)
 
-#	print(Singleton.player2.number_bomb)
-#	$Player.translate(v*delta)
-#	anim_switch("walk")
+			$Player2.move_and_collide(v1*delta)
 
 func _input(event):
 	
-#	if Input.is_key_pressed(KEY_LEFT) :
+
 	if Input.is_action_pressed("ui_left"):
-#		spritedir = "left"
+
 		anim_switch("walkleft")
-#	elif Input.is_key_pressed(KEY_RIGHT) :
+
 	elif Input.is_action_pressed("ui_right"):
-#		spritedir = "right"
+
 		anim_switch("walkright")
-#	if Input.is_key_pressed(KEY_UP) :
+
 	elif Input.is_action_pressed("ui_up"):
-#		spritedir = "up"
+
 		anim_switch("walkup")
-#	elif Input.is_key_pressed(KEY_DOWN):
+
 	elif Input.is_action_pressed("ui_down"):
-#		spritedir = "down"
+
 		anim_switch("walkdown")	
 		
-#	if Input.is_key_pressed(KEY_SPACE):
+
 	if Input.is_action_pressed("Bomb"):
 		
 		if Singleton.player1.number_current_bomb < Singleton.player1.number_bomb:
 			
 			Singleton.player1.add_bomb()
-			
-#			var bomb = Bomb.instance()
-#			bomb.max_bomb = Singleton.player1.range_bomb 
-#			bomb.scale = Vector2(1,1)
-#			bomb.z_index = -1
-#			var g = grid.is_cell_vacant($Player.get_position())
-#
-#			bomb.position = Vector2((g.x*$TileMap.cell_size.x)+$TileMap.cell_size.x/2,(g.y*$TileMap.cell_size.y)+$TileMap.cell_size.y/2)
-#
-#			add_child(bomb)
-	#		print(bomb.get_position())
-	#		var g = grid.is_cell_vacant(bomb.get_position())
-			
-	#		var grid
-	#		var target_pos
-	#		grid = $TileMap
-	##		target_pos = grid.update_child_pos(self)
-	#		target_pos = grid.update_child_pos($Player)
-	#		var pos = get_position()
-	#		var distance_to_target = Vector2(abs(target_pos.x - pos.x),abs(target_pos.y-pos.y))
-	#		print(distance_to_target)
-	#		print(bomb.position)
-	#		list_bomb.append(bomb)
-	#		get_tree().root.print_tree_pretty()
-
+	
 
 #####player2
 	if Input.is_key_pressed(KEY_LEFT) :
-#	if Input.is_action_pressed("ui_left"):
-#		spritedir = "left"
 		anim_switch("walkleft")
 	elif Input.is_key_pressed(KEY_RIGHT) :
-#	elif Input.is_action_pressed("ui_right"):
-#		spritedir = "right"
 		anim_switch("walkright")
 	if Input.is_key_pressed(KEY_UP) :
-#	elif Input.is_action_pressed("ui_up"):
-#		spritedir = "up"
+
 		anim_switch("walkup")
 	elif Input.is_key_pressed(KEY_DOWN):
-#	elif Input.is_action_pressed("ui_down"):
-#		spritedir = "down"
+
 		print("current:",Singleton.player2.number_current_bomb)
 		print("number_bomb:",Singleton.player2.number_bomb)
 		anim_switch("walkdown")	
 		
 	if Input.is_key_pressed(KEY_SPACE):
-#	if Input.is_action_pressed("Bomb"):
+
 		if Singleton.player2.number_current_bomb < Singleton.player2.number_bomb:
 			Singleton.player2.add_bomb()
-			
-#			print("วางแล้ว")
-#			var bomb1 = Bomb1.instance()
-#			Singleton.player2.number_current_bomb +=1
-#			bomb1.max_bomb = Singleton.player2.range_bomb 
-#			bomb1.self_player = Singleton.player2
-#			bomb1.scale = Vector2(1,1)
-#			bomb1.z_index = -1
-#			var g1 = grid.is_cell_vacant($Player2.get_position())
-#	#		bomb.position = Vector2(170,70)
-#			bomb1.position = Vector2((g1.x*$TileMap.cell_size.x)+$TileMap.cell_size.x/2,(g1.y*$TileMap.cell_size.y)+$TileMap.cell_size.y/2)
-#	#		bomb.position = Vector2($Player.get_position().x,$Player.get_position().y)
-#			add_child(bomb1)
-		
+
 	pass
 #
 func anim_switch(animation):
-#	var newanim = str(animation,spritedir)
+
 	if player.get_node("AnimationPlayer").current_animation != "":
-#		player.get_node("AnimationPlayer").play(newanim)
+
 		player.get_node("AnimationPlayer").play(animation)
 
 
